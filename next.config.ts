b61600f990c722fa15782cd31efef1ca61e5e48d
Gihanvs98 <darkcreationsl98@gmail.com>
2026-02-04 16:01:11 +0530

    refactor(menu):changed category drop down

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "via.placeholder.com",
+        hostname: "**",
       },
     ],
   },
